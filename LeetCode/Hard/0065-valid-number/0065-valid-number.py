2663087e92b6cc6c161557819be57ba5e9875a23
Jude Kim <45715424+coshua@users.noreply.github.com>
2025-06-04 13:43:45 -0700

    0065-valid-number_06-04-2025

--- LeetCode/Hard/0065-valid-number/0065-valid-number.py
+++ LeetCode/Hard/0065-valid-number/0065-valid-number.py
@@ -2,36 +2,22 @@ class Solution:
     def isNumber(self, s: str) -> bool:
         dot = False
         digit = False
-        if len(s) == 1 and not s[0].isdigit():
-            return False
+        exp = False
         for i, ch in enumerate(s):
-            if ch in ['-','+'] and i > 0:
-                return False
             if ch.isdigit():
                 digit = True
-
-            if ch == '.':
-                if dot:
+            elif ch in ['+', '-']:
+                if i > 0 and s[i-1] not in ['e','E']:
+                    return False
+            elif ch in ['e', 'E']:
+                if exp or not digit:
+                    return False
+                digit = False
+                exp = True
+            elif ch == '.':
+                if dot or exp:
                     return False
                 dot = True
-            
-            
-            if ch == 'e' or ch == 'E':
-                if not digit:
-                    return False
-                if i == len(s) - 1:
-                    return False
-                j = i + 1
-                if s[j] in ['-','+']:
-                    if j == len(s) - 1:
-                        return False
-                    j += 1
-                while j < len(s):
-                    if not s[j].isdigit():
-                        return False
-                    j += 1
-                return True
-                
-            if not ch.isdigit() and ch not in ['-','+','.']:
+            else:
                 return False
         return digit
