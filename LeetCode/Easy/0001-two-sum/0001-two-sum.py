d394537d6c91cecb31713563d6a2ac34101c82de
Jude Kim <45715424+coshua@users.noreply.github.com>
2025-06-07 00:45:51 -0700

    0001-two-sum_06-07-2025

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        s = {}
+        for i, n in enumerate(nums):
+            if target - n in s:
+                return [i, s[target - n]]
+            s[n] = i
+        
+        return
