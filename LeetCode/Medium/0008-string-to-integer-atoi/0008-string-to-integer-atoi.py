239b05063d9376d6419800c7d20743af21efccb1
Jude Kim <45715424+coshua@users.noreply.github.com>
2025-06-07 00:59:09 -0700

    0008-string-to-integer-atoi_06-07-2025

--- LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -7,7 +7,13 @@ class Solution:
         if s[0] in ['-', '+']:
             pnt += 1
         digits = 0
+        negmax = 2**31
+        posmax = 2**31-1
         while pnt < len(s) and s[pnt].isdigit():
+            if s[0] == '-' and digits > negmax / 10:
+                return -negmax
+            if s[0] != '-' and digits > posmax / 10:
+                return posmax
             digits = digits * 10 + int(s[pnt])
             pnt += 1
         if not digits:
