6c9510a0c96fb40d6af7e6eadfc3d988f7be9937
Jude Kim <45715424+coshua@users.noreply.github.com>
2025-06-10 21:03:59 -0700

    0071-simplify-path_06-10-2025

--- LeetCode/Medium/0071-simplify-path/0071-simplify-path.py
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path.py
@@ -1,26 +1,12 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path = '/' + path + '/'
-        s = re.sub(r'/+', '/', path)
         st = []
-        dirs = []
-        for ch in s:
-            if ch == '/':
-                prev = ''.join(dirs)
-                if prev == '..':
-                    if len(st) > 1:
-                        st.pop()
-                    if len(st) > 1:
-                        st.pop()
-                elif prev != '.':
-                    if prev:
-                        st.append(prev)
-                    st.append('/')
-                dirs.clear()
+        for word in path.split('/'):
+            if word == '..':
+                if st:
+                    st.pop()
+            elif word == '.' or not word:
+                continue
             else:
-                dirs.append(ch)
-        if len(st) > 1 and st[-1] == '/':
-            st.pop()
-        if not st:
-            st.append('/')
-        return ''.join(st)
+                st.append(word)
+        return '/' + '/'.join(st)
