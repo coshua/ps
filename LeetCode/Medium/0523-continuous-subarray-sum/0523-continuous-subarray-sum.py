81f5ef62a1794928986df27545daa39e29df172a
Jude Kim <45715424+coshua@users.noreply.github.com>
2025-06-12 01:24:13 -0700

    0523-continuous-subarray-sum_06-12-2025

--- LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,17 +1,13 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        d = {}
+        d = {0: -1}
         cur = 0
 
         for i, n in enumerate(nums):
-            if n == 0:
-                if i > 0 and nums[i-1] == 0:
-                    return True
             cur = (cur+n)%k
-            if cur == 0 and i > 0:
-                return True
-            if cur in d and i - d[cur] > 1:
-                return True
-            if cur not in d:
+            if cur in d:
+                if i - d[cur] > 1:
+                    return True
+            else:
                 d[cur] = i
         return False
